52f66738ae55fa97e2197af271a0d408c5a63fd0
Daniel Mendalka <daniel.mendalka@gmail.com>
2012-03-20 05:27:37 +0100

    Some refactoring done during excersices

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,7 @@
 module SessionsHelper
 
   def sign_in(user)
+    session[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
     current_user = user
     current_user
@@ -11,6 +12,7 @@ module SessionsHelper
   end
 
   def current_user
+    @current_user ||= user_from_session
     @current_user ||= user_from_remember_token
   end
 
@@ -20,11 +22,17 @@ module SessionsHelper
 
   def sign_out
     current_user = nil
+    session.delete(:user_id)
     cookies.delete(:remember_token)
     current_user
   end
 
   private
+    def user_from_session
+      user_id = session[:user_id]
+      User.find(session[:user_id]) unless user_id.nil?
+    end
+
     def user_from_remember_token
       remember_token = cookies[:remember_token]
       User.find_by_remember_token(remember_token) unless remember_token.nil?
